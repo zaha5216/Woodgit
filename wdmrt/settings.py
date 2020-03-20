191dd3f2edecde5a427c7506978c4e70178c1af9
Zaha5216 <zaha5216@.gmail.com>
2020-03-20 15:54:57 +0300

    my message

--- wdmrt/settings.py
+++ wdmrt/settings.py
@@ -88,13 +88,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
