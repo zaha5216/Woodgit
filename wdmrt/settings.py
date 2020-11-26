613d7d60dfa837deb79126137511e5d2c918a155
Zaha5216 <zaha5216@.gmail.com>
2020-11-26 15:49:04 +0300

    my message

--- wdmrt/settings.py
+++ wdmrt/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'wdmrt.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'de2q3jr9bjdel9',
-        'USER': 'xctouiclzkazsy',
-        'PASSWORD': '36295d4f7daf726f574befdd8b8325c55c2a037570310d09c95da2903abf7485',  # might be empty string ''
-        'HOST': 'ec2-54-225-115-177.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-#
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'de2q3jr9bjdel9',
+#         'USER': 'xctouiclzkazsy',
+#         'PASSWORD': '36295d4f7daf726f574befdd8b8325c55c2a037570310d09c95da2903abf7485',  # might be empty string ''
+#         'HOST': 'ec2-54-225-115-177.compute-1.amazonaws.com',
+#         'PORT': '5432',
 #     }
 # }
+#
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'wdmrt.data'),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
